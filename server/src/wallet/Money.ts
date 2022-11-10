b50a6ae3720a951bee77be6486e80815c4de4724
Paul-Sebastian Manole <brokenthorn@gmail.com>
2022-11-11 01:52:14 +0200

    Added Money and Wallet classes

---
+++ server/src/wallet/Money.ts
@@ -0,0 +1,40 @@
+export class Money {
+  private _units: number = 0;
+  public get units(): number {
+    return this._units;
+  }
+
+  private _cents: number = 0;
+  public get cents(): number {
+    return this._cents;
+  }
+
+  protected constructor(units: number, cents: number) {
+    // add units:
+    this._units += units;
+
+    // add cents:
+    const totalCents = this._cents + cents;
+
+    if (totalCents < 100) {
+      this._cents = totalCents;
+    } else {
+      // leave less than 100 cents in cents:
+      const remainingCents = totalCents % 100;
+      this._cents = remainingCents;
+      // and convert the rest to full units (e.g. dollars, etc.)
+      // and add them to the units pile:
+      const unitsFromCents = Math.floor(totalCents / 100);
+      this._units += unitsFromCents;
+    }
+  }
+
+  public static Create(units: number, cents: number): Money | undefined {
+    if (!Number.isInteger(units) || !Number.isInteger(cents)) return;
+    return new Money(units, cents);
+  }
+
+  public static Zero(): Money {
+    return new Money(0, 0);
+  }
+}
---
+++ server/src/wallet/Wallet.ts
@@ -0,0 +1,95 @@
+import { Money } from "./Money";
+
+type WalletTransactionEvent = {
+  readonly Date: Date;
+  readonly FromOwnerId: string;
+  readonly ToOwnerId: string;
+  readonly Amount: Money;
+};
+
+export class Wallet {
+  private _ownerId: string;
+  private _money: Money;
+  private _transactionEvents: WalletTransactionEvent[];
+
+  /**
+   * Construct a default empty wallet.
+   */
+  constructor(ownerId: string) {
+    if (ownerId.length === 0) throw new Error("ownerId was empty.");
+    this._ownerId = ownerId;
+    this._money = Money.Zero();
+    this._transactionEvents = [];
+  }
+
+  private AddMoney(money: Money): boolean {
+    let units = this._money.units + money.units;
+    let cents = 0;
+
+    const totalCents = this._money.cents + money.cents;
+    if (totalCents < 100) {
+      cents = totalCents;
+    } else {
+      const remainingCents = totalCents % 100;
+      cents = remainingCents;
+      const unitsFromCents = Math.floor(totalCents / 100);
+      units += unitsFromCents;
+    }
+
+    const newMoney = Money.Create(units, cents);
+    if (newMoney) {
+      this._money = newMoney;
+      return true;
+    }
+    return false;
+  }
+
+  private RemoveMoney(money: Money): boolean {
+    if (money.units > this._money.units) return false;
+    if (money.units === this._money.units && money.cents > this._money.cents)
+      return false;
+
+    const units = this._money.units - money.units;
+    const cents = this._money.cents - money.cents;
+
+    const newMoney = Money.Create(units, cents);
+    if (newMoney) {
+      this._money = newMoney;
+      return true;
+    }
+    return false;
+  }
+
+  Receive(money: Money, fromOwnerId: string) {
+    if (fromOwnerId.length === 0) throw new Error("fromOwnerId was empty.");
+
+    this.AddMoney(money);
+
+    this._transactionEvents = [
+      ...this._transactionEvents,
+      {
+        Date: new Date(),
+        FromOwnerId: fromOwnerId,
+        ToOwnerId: this._ownerId,
+        Amount: money,
+      },
+    ];
+  }
+
+  Give(money: Money, toOwnerId: string): boolean {
+    if (toOwnerId.length === 0) throw new Error("toOwnerId was empty.");
+    if (!this.RemoveMoney(money)) return false;
+
+    this._transactionEvents = [
+      ...this._transactionEvents,
+      {
+        Date: new Date(),
+        FromOwnerId: this._ownerId,
+        ToOwnerId: toOwnerId,
+        Amount: money,
+      },
+    ];
+
+    return true;
+  }
+}
