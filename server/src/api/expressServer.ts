796f5b17ccb133726f1296eae7aaa9f3d85a4489
Paul-Sebastian Manole <brokenthorn@gmail.com>
2022-11-15 20:29:37 +0200

    Refactoring for better code organization

--- server/src/api/expressServer.ts
+++ server/src/api/expressServer.ts
@@ -4,42 +4,6 @@ import { logInformation } from "../utils";
 import { expressApp } from "./expressApp";
 
 const server = http.createServer(expressApp);
-
-/**
- * Shuts down the server on a particular process signal,
- * logging that information before it does so.
- * @param signal The process signal that determined the shutdown.
- */
-const shutdownOnSignal = (signal: NodeJS.Signals) => {
-  logInformation(`${signal} signal received. Shutting down server.`);
-  server.close(() => logInformation("Server has shut down."));
-};
-
-const SIGKILL: NodeJS.Signals = "SIGKILL";
-const SIGINT: NodeJS.Signals = "SIGINT";
-const SIGTERM: NodeJS.Signals = "SIGTERM";
-const SIGHUP: NodeJS.Signals = "SIGHUP";
-const SIGUSR1: NodeJS.Signals = "SIGUSR1";
-const SIGUSR2: NodeJS.Signals = "SIGUSR2";
-
-// add process signal handlers:
-process.on(SIGTERM, () => shutdownOnSignal(SIGTERM));
-process.on(SIGKILL, () => shutdownOnSignal(SIGKILL));
-process.on(SIGINT, () => shutdownOnSignal(SIGINT));
-process.on(SIGHUP, () => shutdownOnSignal(SIGHUP));
-process.on(SIGUSR1, () => shutdownOnSignal(SIGUSR1));
-process.on(SIGUSR2, () => shutdownOnSignal(SIGUSR2));
-
-// close and restart the server on internal server errors:
-server.on("error", (e) => {
-  logInformation(`Error: ${e.name}: ${e.message}`);
-  logInformation("Restarting server.");
-
-  setTimeout(() => {
-    server.close(() => listen());
-  }, 3000);
-});
-
 const port = Number(process.env.PORT) || 8080;
 const hostname = process.env.HOSTNAME ?? "localhost";
 
@@ -55,3 +19,38 @@ export const listen = () => {
     logInformation(`Listening at ${addr.address}:${addr.port}`);
   });
 };
+
+// close and restart the server on internal server errors:
+server.on("error", (e) => {
+  logInformation(`Error: ${e.name}: ${e.message}`);
+  logInformation("Restarting server.");
+
+  setTimeout(() => {
+    server.close(() => listen());
+  }, 3000);
+});
+
+const SIGKILL: NodeJS.Signals = "SIGKILL";
+const SIGINT: NodeJS.Signals = "SIGINT";
+const SIGTERM: NodeJS.Signals = "SIGTERM";
+const SIGHUP: NodeJS.Signals = "SIGHUP";
+const SIGUSR1: NodeJS.Signals = "SIGUSR1";
+const SIGUSR2: NodeJS.Signals = "SIGUSR2";
+
+/**
+ * Shuts down the server on a particular process signal,
+ * logging that information before it does so.
+ * @param signal The process signal that determined the shutdown.
+ */
+const shutdownServerOnSignal = (signal: NodeJS.Signals) => {
+  logInformation(`${signal} signal received. Shutting down server.`);
+  server.close(() => logInformation("Server has shut down."));
+};
+
+// add process signal handlers:
+process.on(SIGTERM, () => shutdownServerOnSignal(SIGTERM));
+process.on(SIGKILL, () => shutdownServerOnSignal(SIGKILL));
+process.on(SIGINT, () => shutdownServerOnSignal(SIGINT));
+process.on(SIGHUP, () => shutdownServerOnSignal(SIGHUP));
+process.on(SIGUSR1, () => shutdownServerOnSignal(SIGUSR1));
+process.on(SIGUSR2, () => shutdownServerOnSignal(SIGUSR2));
